af4f883ba18fd2014bd4f138646a7cbb7c7c06ab
Lucien-zy <1255532903@qq.com>
2018-06-07 18:44:17 +0800

    first commit

---
+++ page/network_page.py
@@ -0,0 +1,26 @@
+import os,sys
+from selenium.webdriver.common.by import By
+sys.path.append(os.getcwd())
+from base.base_action import BaseAction
+
+class NetworkPage(BaseAction):
+    more_button=By.XPATH,"//*[contains(@text,'更多')]"
+    network_button=By.XPATH,"//*[contains(@text,'移动网络')]"
+    first_network_button=By.XPATH,"//*[contains(@text,'首选网络类型')]"
+    network_2g_button=By.XPATH,"//*[contains(@text,'2G')]"
+    network_3g_button=By.XPATH,"//*[contains(@text,'3G')]"
+
+    def __init__(self, driver):
+        BaseAction.__init__(self, driver)
+        self.click(self.more_button)
+        self.click(self.network_button)
+
+    def click_first_network(self):
+        self.click(self.first_network_button)
+
+    def click_2g(self):
+        self.click(self.network_2g_button)
+
+    def click_3g(self):
+        self.click(self.network_3g_button)
+
